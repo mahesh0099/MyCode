8810775900564b7b95b366a6a4fdf14a5beba57a
mahesh0099 <41848002+mahesh0099@users.noreply.github.com>
2018-08-28 22:22:50 +0530

    Update App.java
    updated with date

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! this is jenkins class and Github class 12-08-2018";
+    private final String message = "Hello World! this is jenkins class and Github class 27-08-2018";
 
     public App() {}
 
